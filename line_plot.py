1ee458c1179c0b6738513981ae892bbce533b034
Sagattto <42481426+Sagattto@users.noreply.github.com>
2023-06-02 20:07:52 +0900

    Add files via upload

---
+++ line_plot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Prepare the data
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Create the graph
+plt.plot(x, y)
+
+# Set the axis labels
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+
+# Set the graph title
+plt.title('Line Plot')
+
+# Display the graph
+plt.show()
